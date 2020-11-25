75406a1b6ac581cf7cc78cd1005c673bf312011a
Zavizhenetc <zavizhenetc@gmail.com>
2020-11-26 00:51:35 +0300

    разметка, scss, бургерменю

--- src/index.js
+++ src/index.js
@@ -6,4 +6,18 @@ function csl(elem){
 import './index.css';
 
 
+const btn = document.querySelector('.burger-menu');
+const jsMenu = document.querySelector('.burger-menu__wrapper');
  
+btn.addEventListener('click',burger);
+
+function burger (){
+  // evt.preventDefault();
+  btn.classList.toggle('active');
+  jsMenu.classList.toggle('active');
+}
+// $('#sandwichmenu').on('click', function(e) {
+//   e.preventDefault();
+//   $('#sandwichmenu').toggleClass("active");
+//   $('.js_menu').toggleClass("active");
+// });
