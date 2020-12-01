5e53fc79f190d47bf61feb455544ca51dd0bf74e
Zavizhenetc <zavizhenetc@gmail.com>
2020-12-01 12:45:58 +0300

    add markup, scss

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 
-[Посмотреть деплой ](https://zavizhenetc.github.io/testUnitedTraders/ )
+[Посмотреть деплой ](https://zavizhenetc.github.io/testIPGOA/ )
 ***
 1.Клонируем 
 
-``git clone https://github.com/Zavizhenetc/testUnitedTraders.git``
+``git clone https://github.com/Zavizhenetc/testIPGOA.git``
 
 2.переходим в папку с проектом 
 
-``` cd testUnitedTraders```
+``` cd testIPGOA```
 
 3.устанавливаем зависимости 
 
--- src/index.js
+++ src/index.js
@@ -4,20 +4,3 @@ function csl(elem){
 
 
 import './index.css';
-
-
-const btn = document.querySelector('.burger-menu');
-const jsMenu = document.querySelector('.burger-menu__wrapper');
- 
-btn.addEventListener('click',burger);
-
-function burger (){
-  // evt.preventDefault();
-  btn.classList.toggle('active');
-  jsMenu.classList.toggle('active');
-}
-// $('#sandwichmenu').on('click', function(e) {
-//   e.preventDefault();
-//   $('#sandwichmenu').toggleClass("active");
-//   $('.js_menu').toggleClass("active");
-// });
